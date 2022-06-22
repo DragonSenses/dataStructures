204e7b15e088a61f6c0ff0a91cd2f296255fbf6a
DragonSenses <107849653+DragonSenses@users.noreply.github.com>
2022-06-21 21:28:46 -0700

    Add files via upload

---
+++ SinglyLinkedList.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @author kendr
+ * A Linked List is a collection of nodes that form a linear sequence. 
+ * A Singly linked List has each node store two values. 
+ * 	1) Data: a reference to an object that is an element of the sequence
+ *  2) Link: a reference to the next node of the list 
+ */
+public class SinglyLinkedList <E> {
+	
+}
