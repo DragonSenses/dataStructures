e807d5f87dc77832957c461754336cae6bce81c9
DragonSenses <kendrickfrancisco@gmail.com>
2022-06-23 21:48:29 -0700

    Create CircularlyLinkedList.java
    A new data structure to improve upon SingularlyLinkedList

---
+++ CircularlyLinkedList.java
@@ -0,0 +1,14 @@
+/**
+ * A LinkedList in which the next reference of the tail node is 
+ * set to refer back to the head of the list rather than null.
+ *
+ * @author kendr
+ *
+ */
+public class CircularlyLinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
