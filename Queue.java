4151ad2387660187891435b5b072b92fff0f7498
DragonSenses <kendrickfrancisco@gmail.com>
2022-06-26 01:16:44 -0700

    Create Queue.java

---
+++ Queue.java
@@ -0,0 +1,9 @@
+/**
+ * A collection of objects with operations according to the first-in,first-out (FIFO)
+ * principle. Only the element that has been in the queue the longest can be removed.
+ * 
+ * @author kendr
+ */
+public class Queue <E> {
+    
+}
