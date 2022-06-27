7a385a9baa71b272787c3f4d0c591cd5cfeabcd3
DragonSenses <kendrickfrancisco@gmail.com>
2022-06-26 22:41:13 -0700

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -1,6 +1,13 @@
+import java.util.NoSuchElementException;
+
 /**
  * A collection of objects with operations according to the first-in,first-out (FIFO)
  * principle. Only the element that has been in the queue the longest can be removed.
+ * Entities can be inserted on one end, but removed from the other end of the sequence.
+ * 
+ * Implementing a circular array based queue, using modular arithhemetic we use instance 
+ * variable "front" to determine the next open available index, so we can wrap around the 
+ * array. 
  * 
  * @author kendr
  */
@@ -8,12 +15,12 @@ public class Queue <E> {
     private static final int INIT_SIZE = 8;  //Initial size of every default Queue
     
     /**  Instance Variables **/
-    private E[] data;       //Underlying array for element storage
-    private int front = 0;  //Index of the front element
+    private E[] arr;       //Underlying array for element storage
+    private int front = 0;  //Index of the front element, front = (front+1) % SIZE
     private int size = 0;   //Current number of elements
 
     /** Constructors **/ 
-    public Queue() { //Default Constructor;
+    public Queue() { //Default Constructor
         this(INIT_SIZE);
     }
 
@@ -23,10 +30,34 @@ public class Queue <E> {
     @SuppressWarnings("unchecked")
     public Queue(int capacity){ 
         //Generic Array -> instantiate Object array, then narrowing type cast to E[]
-        data = (E[]) new Object[capacity];
+        arr = (E[]) new Object[capacity];
     }
 
     /** Access Methods **/
+    /**
+     * @return number of element within the queue
+     */
+    public int size(){
+        return this.size;
+    }
+
+    /**
+     * @return true if queue is empty, false otherwise
+     */
+    public boolean isEmpty(){
+        return this.size == 0;
+    }
+
+    /**
+     * Returns, but does not remove, the first element of the queue
+     * @return the first element
+     * @throws NoSuchElementException if queue is empty
+     */
+    public E first(){
+        if(isEmpty()) { return new NoSuchElementException("Queue Underflow"); }
+        return arr[front];
+    }
+
     /** Update Methods **/
     /** Helper Methods **/
 
