6f6169503dbb043ecd0cd7f9116667b2c19a13df
DragonSenses <kendrickfrancisco@gmail.com>
2022-06-28 22:38:23 -0700

    Update CircularlyLinkedListTest.java

--- CircularlyLinkedListTest.java
+++ CircularlyLinkedListTest.java
@@ -29,4 +29,98 @@ public class CircularlyLinkedListTest {
         assumeTrue(list.isEmpty()); // Expect an empty list
         assertEquals(0,list.size()); // Expect the size to be 0
     }
+
+    @Test
+    public void addFirstOne(){
+        list.addFirst(1);
+        assertAll("list",
+            () -> assertEquals(false,list.isEmpty()),
+            () -> assertEquals(1,list.size()),
+            () -> assertEquals(1,list.first()),
+            () -> assertEquals(1,list.last())
+        );
+    }
+
+    @Test
+    public void addLastOne(){
+        list.addLast(1);
+        assertAll("list",
+            () -> assertEquals(false,list.isEmpty()),
+            () -> assertEquals(1,list.size()),
+            () -> assertEquals(1,list.first()),
+            () -> assertEquals(1,list.last())
+        );
+    }
+
+    @Test
+    public void removeFirstOneElement(){
+        list.addFirst(1);
+        assertAll("list",
+            () -> assertEquals(false,list.isEmpty()),
+            () -> assertEquals(1,list.size()),
+            () -> assertEquals(1,list.first()),
+            () -> assertEquals(1,list.last())
+        );
+        //After removal we expect empty, 0 size, and null first/last
+        list.removeFirst();
+        assertAll("list",
+        () -> assertEquals(true,list.isEmpty()),
+        () -> assertEquals(0,list.size()),
+        () -> assertEquals(null,list.first()),
+        () -> assertEquals(null,list.last())
+    );
+    }
+
+    @Test
+    public void removeLastFirstElement(){
+        list.addFirst(1);
+        assertAll("list",
+            () -> assertEquals(false,list.isEmpty()),
+            () -> assertEquals(1,list.size()),
+            () -> assertEquals(1,list.first()),
+            () -> assertEquals(1,list.last())
+        );
+        //After removal we expect empty, 0 size, and null first/last
+        list.removeFirst();
+        assertAll("list",
+        () -> assertEquals(true,list.isEmpty()),
+        () -> assertEquals(0,list.size()),
+        () -> assertEquals(null,list.first()),
+        () -> assertEquals(null,list.last())
+    );
+    }
+
+    @Test
+    public void equalsNull(){
+        assertEquals(false,list.equals(null));
+    }
+
+    @Test
+    public void equalsItself(){
+        list.addFirst(1);
+        list.addLast(2);
+        assertEquals(true, list.equals(list));
+    }
+
+    @Test
+    public void equalsItselfOneElement(){
+        list.addFirst(2);
+        assertEquals(true, list.equals(list));
+    }
+
+
+    @Test
+    public void equalsOneElement(){
+        CircularlyLinkedList<Integer> list2 = new CircularlyLinkedList<Integer>();
+        list.addFirst(1);
+        list2.addLast(1);
+        assertEquals(true, list.equals(list2));
+    }
+
+    @Test
+    public void notEquals(){
+        CircularlyLinkedList<Integer> list2 = new CircularlyLinkedList<Integer>();
+        list2.addLast(2);
+        assertEquals(false, list.equals(list2));
+    }
 }
