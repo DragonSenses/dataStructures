f6baf07c889ea8f1930a25a9ec32a148525816d5
DragonSenses <kendrickfrancisco@gmail.com>
2022-06-28 22:33:03 -0700

    Update DoublyLinkedListTest.java

--- DoublyLinkedListTest.java
+++ DoublyLinkedListTest.java
@@ -26,4 +26,97 @@ public class DoublyLinkedListTest {
         assumeTrue(list.isEmpty()); // Expect an empty list
         assertEquals(0,list.size()); // Expect the size to be 0
     }
+
+    @Test
+    public void addFirstOne(){
+        list.addFirst(1);
+        assertAll("list",
+            () -> assertEquals(false,list.isEmpty()),
+            () -> assertEquals(1,list.size()),
+            () -> assertEquals(1,list.first()),
+            () -> assertEquals(1,list.last())
+        );
+    }
+
+    @Test
+    public void addLastOne(){
+        list.addLast(1);
+        assertAll("list",
+            () -> assertEquals(false,list.isEmpty()),
+            () -> assertEquals(1,list.size()),
+            () -> assertEquals(1,list.first()),
+            () -> assertEquals(1,list.last())
+        );
+    }
+
+    @Test
+    public void removeFirstOneElement(){
+        list.addFirst(1);
+        assertAll("list",
+            () -> assertEquals(false,list.isEmpty()),
+            () -> assertEquals(1,list.size()),
+            () -> assertEquals(1,list.first()),
+            () -> assertEquals(1,list.last())
+        );
+        //After removal we expect empty, 0 size, and null first/last
+        list.removeFirst();
+        assertAll("list",
+        () -> assertEquals(true,list.isEmpty()),
+        () -> assertEquals(0,list.size()),
+        () -> assertEquals(null,list.first()),
+        () -> assertEquals(null,list.last())
+    );
+    }
+
+    @Test
+    public void removeLastOneElement(){
+        list.addFirst(1);
+        assertAll("list",
+            () -> assertEquals(false,list.isEmpty()),
+            () -> assertEquals(1,list.size()),
+            () -> assertEquals(1,list.first()),
+            () -> assertEquals(1,list.last())
+        );
+        //After removal we expect empty, 0 size, and null first/last
+        list.removeLast();
+        assertAll("list",
+        () -> assertEquals(true,list.isEmpty()),
+        () -> assertEquals(0,list.size()),
+        () -> assertEquals(null,list.first()),
+        () -> assertEquals(null,list.last())
+    );
+    }
+
+    @Test
+    public void equalsNull(){
+        assertEquals(false,list.equals(null));
+    }
+
+    @Test
+    public void equalsItself(){
+        list.addFirst(1);
+        assertEquals(true, list.equals(list));
+    }
+
+    @Test
+    public void equalsItselfOneElement(){
+        list.addFirst(2);
+        assertEquals(true, list.equals(list));
+    }
+
+
+    @Test
+    public void equalsOneElement(){
+        DoublyLinkedList<Integer> list2 = new DoublyLinkedList<Integer>();
+        list.addFirst(1);
+        list2.addLast(1);
+        assertEquals(true, list.equals(list2));
+    }
+
+    @Test
+    public void notEquals(){
+        DoublyLinkedList<Integer> list2 = new DoublyLinkedList<Integer>();
+        list2.addLast(2);
+        assertEquals(false, list.equals(list2));
+    }
 }
