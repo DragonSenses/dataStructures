2322a06e452675168728be5c8f063be3f3f41ea9
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-03 22:36:21 -0700

    Create List.java

---
+++ List.java
@@ -0,0 +1,21 @@
+/**
+ * List Interface
+ * 
+ * @author kendr
+ */
+public interface List<E> {
+    /**
+     * Returns the number of elements in the list.
+     * 
+     * @return number of elements in the list
+     */
+    int size();
+
+    /**
+     * Tests whether the list is empty.
+     * 
+     * @return true if the list is empty, false otherwise
+     */
+    boolean isEmpty();
+
+}
