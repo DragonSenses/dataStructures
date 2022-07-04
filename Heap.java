db31bcb25e635119e46c8aa96aa61fe23e0675af
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-03 22:02:22 -0700

    Update Heap.java

--- Heap.java
+++ Heap.java
@@ -27,16 +27,14 @@ public class Heap<K,V> {
 	 * @param <V>		Values
 	 */
 	private static class Entry<K, V> {
-		K key;
-		V value;
+		K key; V value;
 		
 		//Constructor
 		private Entry(K key, V value) {
 			this.key = key;
-			this.value = value;
+			this.setValue(value);
 		}
 
-		
 		public K getKey() {
 			return key;
 		}
@@ -45,10 +43,16 @@ public class Heap<K,V> {
 			return value;
 		}
 		
-		public void setValue(V value) {
+		private void setValue(V value) {
 			this.value = value;
 		}
+
+        @Override
+        public String toString(){ 
+            return "<" + getKey() + ", " + getValue() + ">"; 
+        }
 	}
+
     //Member Fields of Heap 
     protected ArrayList<Entry<K,V>> heap = new ArrayList<>();
     private Comparator<K> c; //Comparator allows us to compare the keys in varying orders
