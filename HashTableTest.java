1ad628c32630c26ca586f03b97ebfccb8268489e
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-10 21:09:23 -0700

    Update HashTableTest.java

--- HashTableTest.java
+++ HashTableTest.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.Assert.assertThrows;
@@ -18,7 +17,15 @@ public class HashTableTest {
     HashTable<Integer,Integer> table;
     HashTable<String,String> strTable;
     HashTable<Integer,Integer> emptyTable;
-    HashTable<Integer,Integer> zeroTable;
+    HashTable<String,String> zeroSizedTable;
+    HashTable<String,String> negSizedTable;
+
+    List<String> actualKeys;
+
+    public static final String TEST_KEY = "Key";
+	public static final String TEST_VAL = "Value";
+    public static final Integer KEY = 7;
+    public static final Integer VAL = 16;
 
     //Error Messages
 	public static final String ILLEGAL_ARG_CAPACITY = "Initial Capacity must be non-negative";
@@ -35,6 +42,63 @@ public class HashTableTest {
         table = new HashTable<Integer, Integer>();
         strTable = new HashTable<String,String>();
     }
+
+    @Test
+	public void zeroSizeConstructor(){
+		IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+			()-> zeroSizedTable = new HashTable<String,String>(0,0.75));
+		assertEquals(ILLEGAL_ARG_CAPACITY,e.getMessage());
+	}
+
+	@Test
+	public void negativeSizeConstructor(){
+		IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+			()-> negSizedTable = new HashTable<String,String>(-16,0.75));
+		assertEquals(ILLEGAL_ARG_CAPACITY,e.getMessage());
+	}
+
+	@Test
+	public void negativeloadFactor(){
+		IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+			()-> negSizedTable = new HashTable<String,String>(16,-24.00));
+		assertEquals(ILLEGAL_ARG_LOAD_FACTOR,e.getMessage());
+	}
+
+	@Test
+	public void largeloadFactor(){
+		
+		IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+			()-> negSizedTable = new HashTable<String,String>(16,2.0));
+		assertEquals(ILLEGAL_ARG_LOAD_FACTOR,e.getMessage());
+	}
+
+    @Test
+	public void putNullKey() {
+		IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+			()-> table.put(null, VAL));
+		assertEquals(ILLEGAL_ARG_NULL_KEY,e.getMessage());
+	}
+
+    @Test
+	public void keysEmptyTable(){
+		List<Integer> expected = new ArrayList<Integer>(0);
+		List<Integer> actual = emptyTable.keys();
+		assertEquals(expected,actual);
+	}
+
+    @Test
+	public void keysNonEmptyTable() {
+		List<String> expectedKeys = new ArrayList<>(5);
+		for(int i = 0; i < 5; i++) {
+			// key + i is used to differentiate keys since they must be unique
+			strTable.put(TEST_KEY + i, TEST_VAL + i);
+			expectedKeys.add(TEST_KEY + i);
+		}
+		actualKeys = strTable.keys();
+		// we need to sort because hash map doesn't guarantee ordering
+		Collections.sort(actualKeys);
+		assertEquals(expectedKeys, actualKeys);
+	}
 
 
     @AfterEach
@@ -55,6 +119,6 @@ public class HashTableTest {
         table = null;
         strTable = null;
         emptyTable = null;
-        zeroTable = null;
+        zeroSizedTable = null;
     }
 }
