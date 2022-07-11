6f1aa4427c565edc1cc00465ffb35368d7960833
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-10 20:59:09 -0700

    Update HashTableTest.java

--- HashTableTest.java
+++ HashTableTest.java
@@ -14,5 +14,47 @@ import java.util.Collections;
 import java.util.List;
 
 public class HashTableTest {
+    /** Instance Variables **/
+    HashTable<Integer,Integer> table;
+    HashTable<String,String> strTable;
+    HashTable<Integer,Integer> emptyTable;
+    HashTable<Integer,Integer> zeroTable;
+
+    //Error Messages
+	public static final String ILLEGAL_ARG_CAPACITY = "Initial Capacity must be non-negative";
+	public static final String ILLEGAL_ARG_NULL_KEY = "Keys must be non-null";
+	public static final String ILLEGAL_ARG_LOAD_FACTOR = "Load Factor must be positive";
     
+    @BeforeAll
+    static void initAll() {
+       
+    }
+
+    @BeforeEach
+    void init() {
+        table = new HashTable<Integer, Integer>();
+        strTable = new HashTable<String,String>();
+    }
+
+
+    @AfterEach
+    void tearDown() {
+        List<Integer> keys = table.keys();
+		for(Integer k: keys){
+			table.remove(k);
+		}
+
+		List<String> sKeys = strTable.keys();
+		for(String k: sKeys){
+			strTable.remove(k);
+		}
+	}
+
+    @AfterAll
+    public void tearDownAll() {
+        table = null;
+        strTable = null;
+        emptyTable = null;
+        zeroTable = null;
+    }
 }
