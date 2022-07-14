69bd758ae69c69dd8e527a72c0c21a990fcf3a66
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-13 21:29:07 -0700

    Update LinkedPositionalList.java

--- LinkedPositionalList.java
+++ LinkedPositionalList.java
@@ -2,13 +2,38 @@
  * Implementation of a positional list using a Doubly Linked List
  */
 public class LinkedPositionalList<E> {
+
+    /**
+     * The position interface provides a general abstraction for the
+     * location of an element within a structure. A position acts as 
+     * a marker or token within a broader list. A position p, which is 
+     * associated with some element e in a list L, does not change, 
+     * even if the index of e changes in L due to insertions or 
+     * deletions elsewhere in the list. The only way in which a 
+     * position is invalid is if that position is removed. Even when
+     * we replace the element e stored at p with another element, p
+     * does not change. This allows a position type that serves as 
+     * parameters to some methods and returns values from other
+     * methods of a list
+     */
+    public interface Position<E> {
+
+        /**
+         * Returns the element stored at the position
+         * @return the stored element
+         * @throws IllegalStateException if it is invalid position
+         */
+    
+        E getElement() throws IllegalStateException;
+    }
+
     /**
      * Node of a doubly linked list, which stores a reference to its
      * element and to both the previous and next node in the list. In
      * order to identify locations withn the linked list we set nodes
      * as the positions. 
      */
-    private static class Node<E> {
+    private static class Node<E> implements Position<E> {
         private E element; // reference to the element stored at this node
         private Node<E> prev; // reference to the previous node in the list
         private Node<E> next; // reference to the subsequent node in the list
