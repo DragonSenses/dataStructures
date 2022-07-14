0f98ce89453ddccefc0c6c23cbd650f29bdf7411
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-13 19:59:13 -0700

    Update PriorityQueue.java

--- PriorityQueue.java
+++ PriorityQueue.java
@@ -1,10 +1,33 @@
-
 /**
  * Priority Queue implemented with a Sorted List. 
  * 
  * O(1) - size(), isEmpty(), min(), removeMin()
  * O(n) - insert()
  */
-public class PriorityQueue {
-    
+public class PriorityQueue <K,V> {
+    private DoublyLinkedList<Entry<K,V>> list = new DoublyLinkedList<>();
+
+    private static class Entry<K, V> { //inner Entry Class
+		
+		protected K key;
+		private V value;
+		
+		//Constructor
+		private Entry(K key, V value) {
+			this.key = key;
+			this.value = value;
+		}
+
+		
+		public K getKey() { return key; }
+
+		public V getValue() { return value; }
+		
+		public void setValue(V value) { this.value = value; }
+
+		@Override
+		public String toString(){
+			return "<" + String.valueOf(key) + " , " + String.valueOf(value) + ">";
+		}
+	}
 }
