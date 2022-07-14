470cc010aba42669d8762cc499185a4fdebe6cb4
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-13 20:52:23 -0700

    Update PriorityQueue.java
    Comparator added

--- PriorityQueue.java
+++ PriorityQueue.java
@@ -1,14 +1,47 @@
+import java.util.Comparator;
+
 /**
  * Priority Queue implemented with a Sorted List. 
  * 
  * O(1) - size(), isEmpty(), min(), removeMin()
  * O(n) - insert()
  */
-public class PriorityQueue <K,V> {
+public class PriorityQueue <K,V extends Comparable<K>> {
+    /** Instance Variables **/
     private DoublyLinkedList<Entry<K,V>> list = new DoublyLinkedList<>();
+    Comparator<K> comp = (K k1, K k2) -> compare(k1,k2);
+
+    public PriorityQueue(){
+        super();
+    }
+
+    public PriorityQueue(Comparator<K> c){
+        this.comp = c;
+    }
+
+    /**
+     * Compares Two Entries by natural ordering
+     * @param x First Entry to compare
+     * @param y Second Entry to compare
+     * @return -1 if x < y, 0 if x==y, 1 if x > y
+     */
+    protected int compare(Entry<K,V> x, Entry<K,V> y){
+        return comp.compare(x.getKey(), y.getKey());
+    }
+
+     /**
+     * Compares Two Keys by natural ordering
+     * @param x First key to compare
+     * @param y Second key to compare
+     * @return -1 if x < y, 0 if x==y, 1 if x > y
+     */
+    protected int compare(K x, K y){
+        return comp.compare(x,y);
+    }
+
+
 
     private static class Entry<K, V> { //inner Entry Class
-		
 		protected K key;
 		private V value;
 		
@@ -18,7 +51,6 @@ public class PriorityQueue <K,V> {
 			this.value = value;
 		}
 
-		
 		public K getKey() { return key; }
 
 		public V getValue() { return value; }
@@ -30,4 +62,6 @@ public class PriorityQueue <K,V> {
 			return "<" + String.valueOf(key) + " , " + String.valueOf(value) + ">";
 		}
 	}
+
+
 }
