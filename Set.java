0c9440f6642f6b8281dff2f0e2645688d401f4ce
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-17 04:23:40 -0700

    Create Set.java

---
+++ Set.java
@@ -0,0 +1,11 @@
+/**
+ * Set interface of the Java Collections framework provides the features 
+ * of a mathematical set in Java. Closely related to the map ADT.
+ * 
+ * 1. A set is an unordered collection of elements, without duplicates. 
+ * Elements of a set are like keys of a map, but without any auxiliary
+ * values
+ */
+public class Set {
+    
+}
