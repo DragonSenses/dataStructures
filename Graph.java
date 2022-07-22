2a11fb7faa764faa595228862bcd6cb58d0f2f5a
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-21 18:29:06 -0700

    Create Graph.java

---
+++ Graph.java
@@ -0,0 +1,11 @@
+
+/**
+ * A Graph is a way of representing relationships that exists betwen
+ * pairs of objects. A graph is a set of objects, called vertices, 
+ * together withh a collection of pairwise connections between them, 
+ * called edges. In short, a graph is a collection of vertices and edges. 
+ * 
+ */
+public interface Graph<V,E> {
+
+}
