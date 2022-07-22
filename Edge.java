8e7d82dbc1b076be685de078a834aef3026a7f4e
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-21 20:55:58 -0700

    Update Edge.java

--- Edge.java
+++ Edge.java
@@ -1,3 +1,11 @@
-public class Edge <E> {
+/**
+ * An edge of a graph.
+ */
+public interface Edge <E> {
     
+    /**
+     * Returns the element associated with the edge.
+     * @return the element asscoiated with the edge
+     */
+    E getElement();
 }
