12572513f48f5e7cdc8646651ac5378685d0b2d6
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-05 02:49:50 -0700

    Update QueueTest.java

--- QueueTest.java
+++ QueueTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import java.util.NoSuchElementException;
 
 public class QueueTest {
-    Stack<Integer> stack;
+    Queue<Integer> queue;   // Default Capacity of 8
 
     private static final int repetitions = 4096;
 
@@ -25,88 +25,87 @@ public class QueueTest {
 
     @BeforeAll
     public static void setup(){
-        System.out.println("Stack Unit Testing has begun ...");
+        System.out.println("Queue Unit Testing has begun ...");
     }
 
     @BeforeEach
     public void init(){
-        this.stack = new Stack<Integer>();
+        this.queue = new Queue<Integer>();
     }
 
     /**
-     * Populates the Stack with integers, [1-n]
-     * @param s The Stack to fill
+     * Populates the Queue with integers, [1-n]
+     * @param s The Queue to fill
      * @param n the number of integers to fill it with
      */
-    private static void fill(Stack<Integer> s, int n) {
+    private static void fill(Queue<Integer> s, int n) {
         for(int i = 1; i < n+1; i++){
-            s.push(i);
+            s.enqueue(i);
         }
     }
     
     @Test
-    public void emptyStack(){         //Tests default constructor
-        assumeTrue(stack.isEmpty()); // Expect an empty list
-        assertEquals(0,stack.size()); // Expect the size to be 0
+    public void emptyQueue(){         //Tests default constructor
+        assumeTrue(queue.isEmpty());  // Expect an empty queue
+        assertEquals(0,queue.size()); // Expect the size to be 0
     }
 
     @Test
-    public void pushOne(){
-        stack.push(1);
-        assertAll("stack",
-            () -> assertEquals(false,stack.isEmpty()),
-            () -> assertEquals(1,stack.size())
-        );
-    }
-
-    // Tests the resizing mechanism
-    @Test
-    public void pushNine(){
-        fill(stack, 9);
-        assertAll("stack",
-            () -> assertEquals(false, stack.isEmpty()),
-            () -> assertEquals(9,stack.size())
+    public void addOne(){
+        queue.enqueue(1);
+        assertAll("queue",
+            () -> assertEquals(false,queue.isEmpty()),
+            () -> assertEquals(1,queue.size())
         );
     }
 
     @Test
-    public void pushNinePopNine(){
-        fill(stack, 9);
+    public void addEight(){
+        fill(queue, 8);
         assertAll("stack",
-            () -> assertEquals(9,stack.size()),
-            () -> assertEquals(false, stack.isEmpty()),
-            () -> assertEquals(9,stack.pop()),
-            () -> assertEquals(8,stack.pop()),
-            () -> assertEquals(7,stack.pop()),
-            () -> assertEquals(6,stack.pop()),
-            () -> assertEquals(5,stack.pop()),
-            () -> assertEquals(4,stack.pop()),
-            () -> assertEquals(3,stack.pop()),
-            () -> assertEquals(2,stack.pop()),
-            () -> assertEquals(1,stack.pop()),
-            () -> assertEquals(true, stack.isEmpty()),
-            () -> assertEquals(0,stack.size())
+            () -> assertEquals(false, queue.isEmpty()),
+            () -> assertEquals(8,queue.size())
         );
     }
 
     @Test
-    public void pushNinePopTen(){
-        fill(stack, 9);
-        assertAll("stack",
-            () -> assertEquals(false, stack.isEmpty()),
-            () -> assertEquals(9,stack.size()),
-            () -> assertEquals(9,stack.pop()),
-            () -> assertEquals(8,stack.pop()),
-            () -> assertEquals(7,stack.pop()),
-            () -> assertEquals(6,stack.pop()),
-            () -> assertEquals(5,stack.pop()),
-            () -> assertEquals(4,stack.pop()),
-            () -> assertEquals(3,stack.pop()),
-            () -> assertEquals(2,stack.pop()),
-            () -> assertEquals(1,stack.pop())
+    public void addEightRemoveEight(){
+        fill(queue, 8);
+        assertAll("queue",
+            () -> assertEquals(8,queue.size()),
+            () -> assertEquals(false, queue.isEmpty()),
+            () -> assertEquals(1,queue.dequeue()),
+            () -> assertEquals(2,queue.dequeue()),
+            () -> assertEquals(3,queue.dequeue()),
+            () -> assertEquals(4,queue.dequeue()),
+            () -> assertEquals(5,queue.dequeue()),
+            () -> assertEquals(6,queue.dequeue()),
+            () -> assertEquals(7,queue.dequeue()),
+            () -> assertEquals(8,queue.dequeue()),
+            () -> assertEquals(true, queue.isEmpty()),
+            () -> assertEquals(0,queue.size())
+        );
+    }
+
+    @Test
+    public void addEightRemoveNine(){
+        fill(queue, 8);
+        assertAll("queue",
+            () -> assertEquals(8,queue.size()),
+            () -> assertEquals(false, queue.isEmpty()),
+            () -> assertEquals(1,queue.dequeue()),
+            () -> assertEquals(2,queue.dequeue()),
+            () -> assertEquals(3,queue.dequeue()),
+            () -> assertEquals(4,queue.dequeue()),
+            () -> assertEquals(5,queue.dequeue()),
+            () -> assertEquals(6,queue.dequeue()),
+            () -> assertEquals(7,queue.dequeue()),
+            () -> assertEquals(8,queue.dequeue()),
+            () -> assertEquals(true, queue.isEmpty()),
+            () -> assertEquals(0,queue.size())
         );
         NoSuchElementException e = assertThrows(NoSuchElementException.class,
-            () -> stack.pop());
+            () -> queue.dequeue());
         assertEquals(UNDERFLOW, e.getMessage());
     }
 
