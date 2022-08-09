6de6933abae2b6c796e5a6310e0d90587e04e1e8
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-09 01:33:16 -0700

    Update HashTable.java

--- HashTable.java
+++ HashTable.java
@@ -311,9 +311,9 @@ public class HashTable<Key, Value> {
 		}
 		
 		// Iterate through the ArrayList , and remove null values
-		for(Key k: ring){
-			if(k == null){
-				ring.remove(k);
+		for(int i = 0; i < keys.length; i++){
+			if(keys[i] == null){
+				ring.remove(keys[i]);
 			}
 		}
 		// Return the list of keys
