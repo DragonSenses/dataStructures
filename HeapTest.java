e6258a9b711d390f7c03cfdf9502d7c513c89b73
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-13 08:39:30 -0700

    Update HeapTest.java

--- HeapTest.java
+++ HeapTest.java
@@ -1,8 +1,8 @@
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.Assert.assertThrows;
@@ -20,6 +20,24 @@ public class HeapTest  {
     @BeforeEach
     public void init() {
         this.heap = new Heap<Integer,Integer>();
+    }
+
+    
+    /**
+     * Populates the Heap with Key,Value pairs of integers, [1-n]
+     * @param h The Heap to fill
+     * @param n the number of integers to fill it with
+     */
+    private static void fill(Heap<Integer,Integer> h, int n) {
+        for(int i = 1; i < n+1; i++){
+            h.insert(i,i);
+        }
+    }
+
+    @Test
+    public void emptyHeap(){         //Tests default constructor
+        assumeTrue(heap.isEmpty());  // Expect an empty list
+        assertEquals(0,heap.size()); // Expect the size to be 0
     }
 
     @AfterEach
