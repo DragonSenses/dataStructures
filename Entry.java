c19acb9e08d50fc1380a2cc627347105a532148a
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-29 19:09:42 -0700

    Update equals() for Testing
    Equality of Entries

--- Entry.java
+++ Entry.java
@@ -19,4 +19,22 @@ public class Entry<K, V> {
     public String toString(){
         return "<" + String.valueOf(key) + ", " + String.valueOf(this.getValue()) + ">";
     }
+
+    @Override
+	public boolean equals (Object o){
+        // Is argument a reference to this object?
+		if(o == this) 
+            return true;
+
+        // 2. Check if argument has correct type 
+		if (!(o instanceof Entry)) 
+            return false;
+
+        // 3. Cast argument o to correct type
+        Entry<?,?> e = (Entry<?,?>)o;
+
+        // 4. for each significant field in class, check if field of the argument
+        // matches thhe corresponding field of this object
+		return e.getKey() == key && e.getValue() == value;
+	}
 }
