508a22ab03eb5d97ab86e7eff0ce678fcecedc6b
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-09 18:03:04 -0700

    Update LinkedPositionalTest.java

--- LinkedPositionalTest.java
+++ LinkedPositionalTest.java
@@ -544,6 +544,28 @@ public class LinkedPositionalTest {
         }
     }
 
+    @Test
+    void removeSameDataTwice(){
+        int n = 8;
+        fill(list,n);
+        assertAll("list",
+            () -> assertEquals(false,list.isEmpty()),
+            () -> assertEquals(n,list.size()),
+            () -> assertEquals(1,list.first().getElement()),
+            () -> assertEquals(n,list.last().getElement()),
+            () -> assertEquals(1,list.remove(list.first()))
+        );
+        list.addFirst(1);
+        assertEquals(1,list.first().getElement());
+        assertEquals(1,list.remove(list.first()));
+
+        Position<Integer> p = list.first();
+        for(int k = 2; k < n+1; k++){
+            assertEquals(k, p.getElement());
+            p = list.after(p);
+        }
+    }
+
     // Exception Testing - Many methods validate() the position passed in
     // before, after, addBefore, addAfter, set(), remove()
 
